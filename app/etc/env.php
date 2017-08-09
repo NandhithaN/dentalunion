9ab20222539686a6b66395b693b0cec3ce15d312
Jacky Raimond <j.raimond@inspirationconcepts.nl>
2017-08-09 08:32:08 +0000

    First commit

---
+++ app/etc/env.php
@@ -0,0 +1,62 @@
+<?php
+return array (
+  'backend' => 
+  array (
+    'frontName' => 'du2017_admin',
+  ),
+  'crypt' => 
+  array (
+    'key' => '878075397f919bdb1d2591f61f810a7d',
+  ),
+  'session' => 
+  array (
+    'save' => 'files',
+  ),
+  'db' => 
+  array (
+    'table_prefix' => '',
+    'connection' => 
+    array (
+      'default' => 
+      array (
+        'host' => 'mysqlmaster.du2017.hypernode.io',
+        'dbname' => 'du2017_preinstalled_magento',
+        'username' => 'app',
+        'password' => 'KlT3nRrPhS7H9InkJkakxIetwterJTwh',
+        'model' => 'mysql4',
+        'engine' => 'innodb',
+        'initStatements' => 'SET NAMES utf8;',
+        'active' => '1',
+      ),
+    ),
+  ),
+  'resource' => 
+  array (
+    'default_setup' => 
+    array (
+      'connection' => 'default',
+    ),
+  ),
+  'x-frame-options' => 'SAMEORIGIN',
+  'MAGE_MODE' => 'default',
+  'cache_types' => 
+  array (
+    'config' => 1,
+    'layout' => 1,
+    'block_html' => 1,
+    'collections' => 1,
+    'reflection' => 1,
+    'db_ddl' => 1,
+    'eav' => 1,
+    'customer_notification' => 1,
+    'full_page' => 1,
+    'config_integration' => 1,
+    'config_integration_api' => 1,
+    'translate' => 1,
+    'config_webservice' => 1,
+  ),
+  'install' => 
+  array (
+    'date' => 'Wed, 09 Aug 2017 08:16:47 +0000',
+  ),
+);
